a8bdc21b40f69855967d5a09dfa34b97b398e097
ran-3roads <ckstjd9735@gmail.com>
2020-12-01 22:20:49 +0900

    2nd commit

---
+++ src/Game.java
@@ -0,0 +1,97 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Game extends JFrame {	
+	private ImageIcon []animal = { 
+			new ImageIcon("images/sea1.jpg"),
+			new ImageIcon("images/sea2.jpg"),
+	};
+	Image img0=animal[0].getImage();
+	Image img1=animal[1].getImage();
+	Image Cimg0=img0.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
+	Image Cimg1=img1.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
+	ImageIcon Cicon0 = new ImageIcon(Cimg0);
+	ImageIcon Cicon1 = new ImageIcon(Cimg1);
+
+	final image a = new image(Cicon0);
+	final image b = new image(Cicon1);
+	//틀린 좌표값과 범위
+	static int[] X = {17,250,110,128,185};
+	static int[] Y = {193,298,43,103,68};
+	static final int range = 15;
+	
+	public Game() {
+		  super("환경보호 틀린그림찾기");
+		  MyMouseAdapter cma = new MyMouseAdapter ();
+		  a.addMouseListener(cma);
+		  b.addMouseListener(cma);
+		 
+		  add(a, BorderLayout.WEST);
+		  add(b, BorderLayout.EAST);
+		 
+
+		  setDefaultCloseOperation(EXIT_ON_CLOSE);
+		  setBounds(300, 300, 700, 350);
+		  setVisible(true);
+		 }
+
+	public boolean isRangeof(int index, Point p){
+		  //포인트 X좌표를 입력받아 pointX로 저장
+		  int pointX = p.x;
+		  //포인트 Y좌표를 입력받아 pointX로 저장
+		  int pointY = p.y;
+		  //배열순번과 해당좌표를 입력해 반환
+		  return (pointX >= X[index]-range) && (pointX <= X[index]+range)
+		    && (pointY >= Y[index]-range) && (pointY <= Y[index]+range);
+		 }//isRangeof()
+	class MyMouseAdapter extends MouseAdapter{
+		  public void mouseReleased(MouseEvent e) {
+		   // 마우스클릭한 곳의 x,y좌표값을 얻어와 Point 객체를 생성
+		   Point p = e.getPoint();
+		   if (isRangeof(0,p)) {
+		    image.locate1 = true; //
+		    a.repaint();
+		    b.repaint(); //양쪽모두 동그라미 표시를 위한 repaint
+
+		   } else if (isRangeof(1,p)) {
+		    image.locate2 = true;
+		    a.repaint();
+		    b.repaint();
+
+		   } else if (isRangeof(2,p)) {
+		    image.locate3 = true;
+		    a.repaint();
+		    b.repaint();
+
+		   } else if (isRangeof(3,p)) {
+		    image.locate4 = true;
+		    a.repaint();
+		    b.repaint();
+
+		   } else if (isRangeof(4,p)) {
+		    image.locate5 = true;
+		    a.repaint();
+		    b.repaint();
+		    
+		   }
+		   // 틀린부분을 다 찾으면 메세지 출력
+		   
+		   if (image.locate1 == true && image.locate2 == true && image.locate3 == true && image.locate4 == true && image.locate5 == true) {
+		    JOptionPane.showMessageDialog(null, "축하합니다.");
+		    //다 완료하면 다시 시작을 위해 동그라미 갱신
+		    image.locate1 = false;
+		    image.locate2 = false;
+		    image.locate3 = false;
+		    image.locate4 = false;
+		    image.locate5 = false;
+		    a.repaint();
+		    b.repaint();
+		   }// if end
+		  }// mouseReleased()
+		 }
+	public static void main(String[] args) {
+		new Game();
+	}
+}
---
+++ src/image.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class image extends JPanel{
+	
+	private JLabel img;
+	
+	static boolean locate1 = false;
+	static boolean locate2 = false;
+	static boolean locate3 = false;
+	static boolean locate4 = false;
+	static boolean locate5 = false;
+	public image(ImageIcon a) {
+		img = new JLabel(a);
+		add(img);
+		}
+	public void paint(Graphics g) {
+		super.paint(g);
+		  // 동그라미 색깔 빨강으로 표시
+		g.setColor(Color.RED);
+		  // 틀린그림부분에 동그라미 그리기
+		if (locate1 == true) {
+			g.drawOval(7, 180, 40, 40); //등딱지 
+			}
+		if (locate2 == true) {
+			g.drawOval(240, 285, 30, 30);// 오른쪽 땅
+		  }
+		if (locate3 == true) {
+		    g.drawOval(100, 30, 50, 50);  //팔
+		  }
+		if (locate4 == true) {
+		    g.drawOval(118, 90, 50, 50); // 볼따구
+		  }
+		if (locate5 == true) {
+		    g.drawOval(175, 55, 50, 50);// 해파리
+		  }
+		 }
+}
